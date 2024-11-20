81171c832c520667726fa5f83dd0eec9dbedbce2
he.jianbo <he.jianbo@trs.com.cn>
2024-11-20 20:44:17 +0800

    chore: bump version to v1.1.10

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -63,7 +63,7 @@ export default defineNuxtConfig({
           async: true,
         },
         {
-          children: 'console.log("Tech Blog Version:", "v1.1.9")',
+          children: 'console.log("Tech Blog Version:", "v1.1.10")',
         },
       ],
     },
