05ece625e6a321888021dd99b31a9ed6a82b2a8c
he.jianbo <he.jianbo@trs.com.cn>
2024-11-20 18:42:48 +0800

    chore: update version display to v1.1.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -63,7 +63,7 @@ export default defineNuxtConfig({
           async: true,
         },
         {
-          children: 'console.log("Tech Blog Version:", "v1.1.0")',
+          children: 'console.log("Tech Blog Version:", "v1.1.1")',
         },
       ],
     },
